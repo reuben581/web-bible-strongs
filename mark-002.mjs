791baf8651f8e34140abebdd328b2cfdc2a01091
F1LT3R <F1LT3R@users.noreply.github.com>
2023-10-18 10:49:54 -0400

    Saving work from Mark 2

--- mark-002.mjs
+++ mark-002.mjs
@@ -1,12 +1,113 @@
-export default [
-    {
-        chapter: 1,
-        verse: 1,
-        word: 1
-    },
-    {
-        chapter: 1,
-        verse: 1,
-        word: 2
-    }
-];
+[
+{   chapter: 2,
+    verse: 1,
+    word: 1,
+    "greek": "Καὶ",
+    "english": "When",
+    "strongs": "G2532",
+    "grammar": "CONJ"
+},
+
+{
+    "number": 345,
+    "greek": "ἐν",
+    "english": "he",
+    "strongs": "G1722",
+    "grammar": "PREP"
+}
+{
+    "number": 346,
+    "greek": "τῷ",
+    "english": "entered",
+    "strongs": "G3588",
+    "grammar": "T-DSM"
+}
+{
+    "number": 347,
+    "greek": "πάλιν",
+    "english": "again",
+    "strongs": "G3825",
+    "grammar": "ADV"
+}
+{
+    "number": 348,
+    "greek": "Καπερναούμ",
+    "english": "into",
+    "strongs": "G2584",
+    "grammar": "N-PRI"
+}
+{
+    "number": 349,
+    "greek": "δι’",
+    "english": "Capernaum",
+    "strongs": "G1223",
+    "grammar": "PREP"
+}
+{
+    "number": 350,
+    "greek": "ἡμερῶν",
+    "english": "after",
+    "strongs": "G2250",
+    "grammar": "N-GPF"
+}
+{
+    "number": 351,
+    "greek": "ἤκουσται",
+    "english": "some",
+    "strongs": "G191",
+    "grammar": "V-RPI-3S"
+}
+{
+    "number": 352,
+    "greek": "ὅτι",
+    "english": "days,",
+    "strongs": "G3754",
+    "grammar": "CONJ"
+}
+{
+    "number": 353,
+    "greek": "ἐν",
+    "english": "it was heard",
+    "strongs": "G1722",
+    "grammar": "PREP"
+}
+{
+    "number": 354,
+    "greek": "οἴκῳ",
+    "english": "that",
+    "strongs": "G3624",
+    "grammar": "N-DSM"
+}
+{
+    "number": 355,
+    "greek": "ἐστίν.",
+    "english": "he was",
+    "strongs": "G1510",
+    "grammar": "V-PXI-3S"
+}
+
+{
+    "number": 356,
+    "greek": "Καὶ",
+    "english": "at",
+    "strongs": "G2532",
+    "grammar": "CONJ"
+}
+{
+    chapter: 2,
+    verse: 1,
+    word: 1,
+    "greek": "συνήχθησαν",
+    "english": "home.",
+    "strongs": "G4863",
+    "grammar": "V-API-3P"
+}
+
+{
+    chapter: 2,
+    verse: 2,
+    word: 1,
+}
+
+
+   
